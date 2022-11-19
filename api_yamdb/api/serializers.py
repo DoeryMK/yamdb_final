4874966956eaaf69f25673f7cd5abba573d3f413
Daria Mukhamedzhanova <klimovadr@gmail.com>
2022-11-19 13:45:29 +0300

    v0.1

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,9 +1,8 @@
 import datetime as dt
 
+from api.validators import UserDataValidation
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
-
-from api.validators import UserDataValidation
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
 
